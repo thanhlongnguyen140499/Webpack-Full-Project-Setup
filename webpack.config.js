8cf282f86806c4ae42eb9b1e1d598bcac8f6d5d4
Long (Edward) T. NGUYEN <edward@enclave.vn>
2023-05-09 15:23:44 +0700

    dev: update - browserslist

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 let mode = "development";
+let target = "web";
 
 if (process.env.NODE_ENV === "production") {
   mode = "production";
+  target = "browserslist";
 }
 
 module.exports = {
   mode: mode,
+  target: target,
 
   module: {
     rules: [
@@ -19,7 +22,7 @@ module.exports = {
         },
       },
       {
-        test: /\.s?css$/i,
+        test: /\.(s[ac]|c)ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
           "css-loader",
