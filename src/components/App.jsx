ed3b305359b37c6d6997a39a1c773d99ccec5a3c
Long (Edward) T. NGUYEN <edward@enclave.vn>
2023-05-11 10:44:44 +0700

    dev: webpack with images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import "../styles/index.scss";
 
 import React from "react";
 import Recipes from "./Recipes";
+import hero from "../images/super-hero-2.jpeg";
 
 function App() {
   return (
@@ -11,9 +12,9 @@ function App() {
         <section>
           <h1>Oh Hi, React</h1>
         </section>
+        <img src={hero} alt="super-hero" width="250px"></img>
+        <Recipes />
       </main>
-
-      <Recipes />
     </>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,21 @@ module.exports = {
   mode: mode,
   target: target,
 
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+
   module: {
     rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+        // parser: {
+        //   dataUrlCondition: {
+        //     maxSize: 30 * 1024,
+        //   },
+        // },
+      },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
